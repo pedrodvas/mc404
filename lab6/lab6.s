e51a8aa0ec08b96f626e49706de1fe1cec296f1d
pedrodvas <pedro.d.vas@gmail.com>
2023-09-17 20:11:32 -0300

    começou a fazer as funções de ascii pra valor e depois para mover para os registradores

--- lab6/lab6.s
+++ lab6/lab6.s
@@ -1,10 +1,31 @@
 /*
 program plan:
-After receiving the 4x(4byte) inputs, the program will tranfer the value from each input into a register,
+    After receiving the 4x(4byte) inputs, 
+the program will tranfer the value from each input into a register, one at a time,
+converting from ascii into value
 register_temporary = b0*1000; register0 = r0 + rt
 register_temporary = b1*100; register0 = r0 + rt
 register_temporary = b2*10; register0 = r0 + rt
 register_temporary = b3*1; register0 = r0 + rt
+    after that the square root will be calculated 
+using the babylonian method iterating 10 times
+    and then the value from the register will be transfered to the output adress
+by taking the remainder from 1 to 100 and storing each result in one byte for
+the output address
+*/
+/*
+registers:
+ra = stores the address for jalr
+t0 = used to store each byte from the input
+t1 = stores the power of 10 that will multiply t0
+t2 = result of multiplication, then its added into a1
+s0 = will store the address from the input_ascii
+s1 = will store the adress from the output
+s2 = address from input_values
+s3 = stores how many bytes from the input were read
+a0 = stores the value from each byte
+a1 = stores the input before the square root
+a2 = stores the value after each iteration from babylonian method
 */
 .data
 string:  .asciz "Hello! It works!!!\n"
@@ -12,6 +33,7 @@ string:  .asciz "Hello! It works!!!\n"
 .bss
 input_address: .skip 20 # 20 byte buffer for the input
                         # AAAA BBBB CCCC DDDD
+input_value: .skip 20
 output_address: .skip 20#same format for the input
 
 .text
@@ -21,6 +43,10 @@ start:
     jal main
 
 main:
+    add a0, x0, x0  #clears the register that stores bytes read
+    la s0, input_address
+    la s1, output_address
+    la s2, input_value
     jal read
     jal write_input
 
@@ -46,3 +72,13 @@ write_padrao:
     li a2, 19           # size
     li a7, 64           # syscall write (64)
     ecall    
+
+ascii_to_value:
+    lbu t0, 0(s0)
+    addi t0, t0, -48
+    #parou aqui na hora de implementar a função de ascii->valor
+
+input_to_register:  #will take each four byte input and move to a register
+    lbu t0, 0(s0)
+    addi t1, x0, 1000
+    
