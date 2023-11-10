d55efb713b53cd81eac2eb1f4acc5de5cea39962
pedrodvas <pedro.d.vas@gmail.com>
2023-11-10 13:40:44 -0300

    busca em arvore binaria terminada

--- lab10b/lab10b.s
+++ lab10b/lab10b.s
@@ -14,23 +14,32 @@ recursive_tree_search:
     #a1 = value
 
     addi a3, x0, 1  #a3 will be depth
+    jal recursion
+    addi a0, x0, 0
+    jalr x0, ra, 0
+    
 
     recursion:
         lw t0, 0(a0)
         beq t0, a1, return
 
         addi sp, sp, -4
-        sw a0, 0(sp)
+        sw a0, 0(sp)    #original root stored
         addi a0, a0, 4  #a0 = a0->left
         addi sp, sp, -4
-        sw ra, 0(sp)
+        sw ra, 0(sp)    #stores return address
         addi a3, a3, 1  #changes the height of the tree
-        jal recursion
+        beq x0, a0, skip_left   #skips the left when 
+        #address is null
+        jal recursion   #check left tree
+
+        skip_left:
         addi a3, a3, -1
         lw ra, 0(sp)
         addi sp, sp, 4
         lw a0, 0(sp)
         addi sp, sp, 4
+
 
         addi sp, sp, -4
         sw a0, 0(sp)
@@ -38,7 +47,10 @@ recursive_tree_search:
         addi sp, sp, -4
         sw ra, 0(sp)
         addi a3, a3, 1  #changes the height of the tree
+        beq x0, a0, skip_right
         jal recursion
+
+        skip_right:
         addi a3, a3, -1
         lw ra, 0(sp)
         addi sp, sp, 4
@@ -48,8 +60,7 @@ recursive_tree_search:
 
         jalr x0, ra, 0
     
-        #falta fazer o pra quando não achar
+        
     return:
-
         mv a0, t0
         jalr x0, ra, 0
